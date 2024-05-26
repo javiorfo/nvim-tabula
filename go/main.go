51e01002c32e9ad21af43730dc726a7b376f35a2
Javier Orfo <javierorfo@prontonmail.com>
2024-05-26 02:13:36 -0300

    column length

--- go/main.go
+++ go/main.go
@@ -3,50 +3,70 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	_ "github.com/lib/pq"
+	"log"
 )
 
+type Pair struct {
+	Name   string
+	Length int
+}
+
+type QueryResult struct {
+	header map[int]Pair
+	rows   map[int][]Pair
+}
+
 func main() {
-    // Establish a connection to the PostgreSQL database
-    connStr := "user=admin dbname=db_dummy password=admin host=localhost sslmode=disable"
-    db, err := sql.Open("postgres", connStr)
-    if err != nil {
-        panic(err)
-    }
-    defer db.Close()
+	connStr := "user=admin dbname=db_dummy password=admin host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-    // Perform a select query
-    rows, err := db.Query("SELECT id as hola, info FROM dummies")
-    if err != nil {
-        panic(err)
-    }
-    defer rows.Close()
+	rows, err := db.Query("SELECT id, info FROM dummies")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
 
-    // Get column names
 	columns, err := rows.Columns()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Create a slice of interface{} to store the values
+	queryResult := QueryResult{
+		make(map[int]Pair),
+		make(map[int][]Pair),
+	}
+	for i, value := range columns {
+		queryResult.header[i+1] = Pair{value, len(value)}
+	}
+
 	values := make([]any, len(columns))
 	for i := range values {
-		var value any
+		var value interface{}
 		values[i] = &value
 	}
 
-	// Iterate over the rows
+	rowNr := 1
 	for rows.Next() {
 		err := rows.Scan(values...)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		// Process the values dynamically based on their types
-		for i, value := range values {
-			fmt.Printf("%s: %v\n", columns[i], *value.(*any))
+		for _, value := range values {
+			val := *value.(*interface{})
+			valor := fmt.Sprintf("%v", val)
+			queryResult.rows[rowNr] = append(queryResult.rows[rowNr], Pair{valor, len(valor)})
+			if queryResult.header[rowNr].Length < len(valor) {
+				queryResult.header[rowNr] = Pair{queryResult.header[rowNr].Name, len(valor)}
+			}
 		}
+		rowNr++
 	}
+	fmt.Println(queryResult.header)
+	fmt.Println(queryResult.rows)
 }
-
